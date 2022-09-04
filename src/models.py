e15c6e1c9c32ea7956852c305035e32487c2e7f0
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-09-04 23:32:01 +0000

    intital commit but complete

--- src/models.py
+++ src/models.py
@@ -23,11 +23,33 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
+    favorites = Column(String(255))
+    favorites = relationship(Favorites)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
     def to_dict(self):
         return {}
+
+class People(Base):
+    __tablename__ = 'people'
+    id= Column(Integer, primary_key=True)
+    name= Column(String, nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id= Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id= Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
